fa5cdde560d08a12a3a6d42f2d99baf794ff9ed2
Barba-Q <46282569+Barba-Q@users.noreply.github.com>
2021-12-22 15:09:58 +0100

    Update qtilewalldog.sh

--- qtilewalldog.sh
+++ qtilewalldog.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-
-
 checksize()
 {
-filename=/home/ingo/Bilder/lxqt.png
+
+# path to you wallpaper goes here-> must be identical to the path given in the config.py
+filename=/your/wallpaper/here.png
 
 filesize=$(du -b "$filename")
 while true;     do
         sleep 10
         filesizechange=$(du -b "$filename")
-        if [ "$filesize" -lt "$filesizechange" ]; then
-                xdotool key super+strg+r
+        if [ "$filesize" = "$filesizechange" ]; then
+                echo "still the same"
+                else
+                xdotool key super+control+r
            exit 
         fi
 
